a27b1beb13de3e39e9bf402fde0e874f1ba7cc5b
Kiran Gadhavi <kirangadhavi@hotmail.com>
2024-05-15 09:30:41 +0100

    Initial setup commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+function App() {
+  
+
+  return (
+    <>  
+      <div>
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</div>
+    </>
+  )
+}
+
+export default App
