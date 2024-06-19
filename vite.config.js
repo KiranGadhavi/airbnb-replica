a7b1f00d1df6ebbeb350985775ced485ff64218c
Kiran Gadhavi <kirangadhavi@hotmail.com>
2024-06-19 12:26:23 +0100

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dist/',
   plugins: [react()],
-  base: '/airbnb-replica/',
   build: {
-    outDir: 'dist',
+    outDir: './dist',
     assetsDir: 'assets'
   }
 })
