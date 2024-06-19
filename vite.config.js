b57ccdf7061764a4281b14fb7a08b5355c61c8bd
Kiran Gadhavi <kirangadhavi@hotmail.com>
2024-06-19 20:54:39 +0100

    changes on package and vite-config file

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist/',
+  base: '/airbnb-replica/',
   plugins: [react()],
-  build: {
-    outDir: './dist',
-    assetsDir: 'assets'
-  }
+  
 })
