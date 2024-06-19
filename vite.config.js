9c2122d4146718350cdb38681285b9ad91744711
Kiran Gadhavi <kirangadhavi@hotmail.com>
2024-06-19 15:34:14 +0100

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dist/',
   plugins: [react()],
+  build: {
+    outDir: './dist',
+    assetsDir: 'assets'
+  }
 })
